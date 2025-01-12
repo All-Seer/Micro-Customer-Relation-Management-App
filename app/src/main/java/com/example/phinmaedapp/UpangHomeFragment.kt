0019684c736d737888380e63d92bf4c072955e6a
AllSeer <xziannquagmire@gmail.com>
2025-01-12 17:37:04 +0800

    Added Upang Home + Personal Details

--- app/src/main/java/com/example/phinmaedapp/UpangHomeFragment.kt
+++ app/src/main/java/com/example/phinmaedapp/UpangHomeFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import com.example.phinmaedapp.databinding.FragmentUpangHomeBinding
@@ -19,29 +17,7 @@ class UpangHomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentUpangHomeBinding.inflate(inflater, container, false)
-        spinnerGender = binding.root.findViewById(R.id.spinnerGender)
-        val genderOptions = listOf("Select Gender", "Male", "Female", "Other")
-        val adapter =
-            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, genderOptions)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinnerGender.adapter = adapter
-        spinnerGender.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                val selectedGender = parent.getItemAtPosition(position) as String
-                if (selectedGender == "Select Gender") {
-                    return
-                }
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>) {
-            }
-        }
-        return binding.root
+        return inflater.inflate(R.layout.fragment_upang_home, container, false)
     }
+
 }
