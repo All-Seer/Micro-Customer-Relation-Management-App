9e0dd86f2a4571d764845c5627cc0ee8f30dab5e
AllSeer <xziannquagmire@gmail.com>
2025-01-15 21:30:09 +0800

    APP NAME + LOGO UPDATED

--- app/src/main/java/com/example/phinmaedapp/UpangSchoolMap.kt
+++ app/src/main/java/com/example/phinmaedapp/UpangSchoolMap.kt
@@ -1,10 +1,12 @@
 package com.example.phinmaedapp
 
 import android.animation.ObjectAnimator
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.VideoView
 import androidx.fragment.app.Fragment
 import com.example.phinmaedapp.databinding.FragmentUpangSchoolMapBinding
 
@@ -12,6 +14,9 @@ class UpangSchoolMap : Fragment() {
 
     private var _binding: FragmentUpangSchoolMapBinding? = null
     private val binding get() = _binding!!
+    private lateinit var videoView: VideoView
+    private var currentPosition: Int = 0
+    private var isVideoPlaying: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,7 +28,16 @@ class UpangSchoolMap : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        videoView = binding.upangMapVideo
+        val videoPath = "android.resource://" + activity?.packageName + "/" + R.raw.phinma_upang_facilities
+        val uri = Uri.parse(videoPath)
+        videoView.setVideoURI(uri)
+        videoView.setOnPreparedListener { mediaPlayer ->
+            mediaPlayer.setVolume(0f, 0f)
+            videoView.start()
+        }
         scrollHorizontallySlowly()
+        scrollHorizontallySlowlyUrdaneta()
     }
     private fun scrollHorizontallySlowly() {
         val scrollView = binding.horizontalScrollView
@@ -35,8 +49,43 @@ class UpangSchoolMap : Fragment() {
             start()
         }
     }
+
+    private fun scrollHorizontallySlowlyUrdaneta() {
+        val scrollView = binding.horizontalScrollViewUrdaneta
+        val scrollAmount = 9000
+        val duration = 20000L
+
+        ObjectAnimator.ofInt(scrollView, "scrollX", scrollAmount).apply {
+            this.duration = duration
+            start()
+        }
+    }
+
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    override fun onPause() {
+        super.onPause()
+        if (isVideoPlaying) {
+            currentPosition = videoView.currentPosition
+            videoView.pause()
+            isVideoPlaying = false
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        if (!isVideoPlaying) {
+            videoView.seekTo(currentPosition)
+            videoView.start()
+            isVideoPlaying = true
+        }
+    }
+    override fun onStop() {
+        super.onStop()
+        if (isVideoPlaying) {
+            currentPosition = videoView.currentPosition
+        }
     }
 }
